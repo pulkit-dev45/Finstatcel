c03aa6182dd8ceb39e84b394f81e7e8dc2496930
Pulkit <shahpulkit@gmail.com>
2026-06-18 21:41:06 +0530

    first commit

--- statementConverter/urls.py
+++ statementConverter/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('MyClientOnlyAccessIt92057/', admin.site.urls),
     path('', include('converter.urls')),
 ]
 
